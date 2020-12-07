ad39920b52db8a9826bc9f4b67d2deb65a7b3bb1
Maratovision <maratovision@gmail.com>
2020-12-07 20:36:15 +0600

    bd_of_users

---
+++ main.py
@@ -0,0 +1,14 @@
+with open("database.txt", "w") as bd:
+    bd.readlines()
+
+# def auth(username, password):
+#     if 8 <= len(username) >= 16:
+#         print("Добро пожаловать!")
+#     else:
+#         print('Не соответсвует длина имени!')
+#     if 8 <= len(password) >= 16:
+#         print("Добро пожаловать!")
+#     else:
+#         print("Не соотвествует длина пароля")
+#
+# auth("username123", "password123")
